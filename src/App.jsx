9ea8687f4589082702414e7705420082a5f27834
slon2plus <9857752669@mail.ru>
2022-03-10 04:03:02 -0800

    add files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+export function App() {
+  return (
+    <div className="section">
+      <div className="phone" />
+      <div className="tablet" />
+      <div className="desctop" />
+      <h1 className="heading">Адаптивная верска</h1>
+    </div>
+  );
+}
